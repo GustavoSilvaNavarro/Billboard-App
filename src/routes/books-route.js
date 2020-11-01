7b037ed68b234afd826066bbe13677f1a9c57111
Gustavo Silva <Gustavo Silva>
2020-11-01 19:59:22 -0300

    Cuarta Version de la libreria de Libros Se agrego todo el BackEnd con todas las funcionalidades CRUD (Create, Read, Update, Delete)

--- src/routes/books-route.js
+++ src/routes/books-route.js
@@ -65,14 +65,74 @@ router.post('/new', /*upload.single('cover'), */async (req, res) => { //como est
     saveCover(book, req.body.cover); //requiero encoded json y lo llamare con req.body.cover ya que over es el name de mi input en el formulario html
     try {
         const newBook = await book.save();
-        //res.redirect(`/books/${newBook.id}`);
-        res.redirect('/books');
+        res.redirect(`/books/${newBook.id}`);
     } catch {
         /*
         if (book.coverImageName != null) {// solo quiero llamar la funcion si es que tengo un coverImageName sino no
             removeBookCover(book.coverImageName); //YA QUE SINO TENGO UN COVERIMAGENAME NO TENDRIA SENTIDO
         } */
         renderNewPage(res, book, true);
+    }
+});
+
+//SHOW SINGLE BOOK
+router.get('/:id', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id).populate('author').exec(); //PARA VER LA INFORMACION DE CADA LIBRO SABEMOS QUE ESTA ESTA LINKEADA A ALGUN AUTOR POR ESO CON LA FUNCION POPULATE QUE INDICA QUE CAMINOS ESTA LINKEADO EL LIBRO ES DECIR EL LIBOR ESTA LINKEADO AL DOCUEMNTO LLAMADO AUTHOR Y QUIERO TRAER ESA INFORMACION
+        res.render('books/show-book.html', { book });
+    } catch {
+        res.redirect('/');
+    }
+});
+
+//UPDATE OR EDIT BOOK
+//Get Edit Form
+router.get('/:id/edit', async (req, res) => {
+    try {
+        const book = await Book.findById(req.params.id);
+        renderEditPage(res, book);
+    } catch {
+        res.redirect('/');
+    }
+});
+
+//Update the Book using Put
+router.put('/:id/edit', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id);
+        book.title = req.body.title;
+        book.author = req.body.author;
+        book.publishDate = new Date(req.body.publishDate);
+        book.description = req.body.description;
+        book.pageCount = req.body.pageCount;
+        if(req.body.cover != null && req.body.cover !== '') { // si cover existe entonces ejecuto la funcion
+            saveCover(book, req.body.cover); //lo hago con el fin de que al editar en el formulario la imagen sale vacia por lo tanto no quiero editar ese documento con una imagen vacia en caso no edite el cover entonces este no se sobreescribira o editara en la base de datos
+        }
+        await book.save();
+        res.redirect(`/books/${book.id}`);
+    } catch {
+        if (book != null) {
+            renderEditPage(res, book, true); //en caso falle el guardado quiero que lo redireccione denuevo a la pagina
+        } else {
+            res.redirect('/');
+        }
+    }    
+});
+
+//DELETE BOOK
+router.delete('/:id', async (req, res) => {
+    let book
+    try {
+        book = await Book.findById(req.params.id);
+        await book.remove();
+        res.redirect('/books');
+    } catch {
+        if (book != null) {
+            res.render('books/show-book.html', { book, errorMessage: 'Could not remove Bool!!'} );
+        } else {
+            res.redirect('/');
+        }
     }
 });
 
@@ -86,8 +146,14 @@ function removeBookCover (fileName) {
 };
 */
 
-//REUSABLE FUNCTION (Contains all logic in one place even from two different routes)
+//EDIT FUNCTION
+async function renderEditPage (res, book, hasError = false) { //coloco hasError en false porque no habra error solo en caso haya se coloca en true
+    renderFormPage(res, book, 'edit', hasError); //de esta manera minimiso la cantidad de codigo
+};
+
+//RENDER FUNCTION (Contains all logic in one place even from two different routes)
 async function renderNewPage (res, book, hasError = false) { //coloco hasError en false porque no habra error solo en caso haya se coloca en true
+    /* COMO YA HICE UNA FUNCION REUSABLE LO PUEDO RESUMIR ASI
     try {
         const authors = await Author.find();
         //const book = new Book(); cuando traigo a parte la funcion no necesito esta linea ya que se la estoy pasando desde mi fucnion puede ser un nuevo book o uno existente
@@ -99,6 +165,8 @@ async function renderNewPage (res, book, hasError = false) { //coloco hasError e
     } catch {
         res.redirect('/books');
     }
+    */
+   renderFormPage(res, book, 'new', hasError);
 };
 
 //RECIVE AND SAVE OUR ENCODED CODE FOR THE IMAGE FILE
@@ -110,6 +178,25 @@ function saveCover(book, coverEncoded) {
     if (cover != null && imageMimeTypes.includes(cover.type)) { //hago una segunda validacion para verificar que no hay un string vacio o en caso haya una mala forma de javascript y verificar que el archivo o formato de archivo sea el correcto
         book.coverImage = new Buffer.from(cover.data, 'base64'); //guardo la info en coverImage pero no la puedo guardar asi nomas la transoformo en Buffer para eso le doy el dato y y en que tipo esta en este caso base64 encoded
         book.coverImageType = cover.type;
+    }
+};
+
+//REUSABLE FUNCTION (Repetitive Function)
+async function renderFormPage (res, book, form, hasError = false) { //coloco hasError en false porque no habra error solo en caso haya se coloca en true
+    try {
+        const authors = await Author.find();
+        //const book = new Book(); cuando traigo a parte la funcion no necesito esta linea ya que se la estoy pasando desde mi fucnion puede ser un nuevo book o uno existente
+        const params = { authors, book }; //para poder crear dinamicamente este menaje de error creo esta variable Y LUEGO QUIERO AGREGAR A ESTE PARAMS UN ERROR DE MANERA DINAMICA
+        if (hasError) { //de esta manera creo mi error de manera dinamica solo cuando haya algun error
+            if (form === 'edit') {
+                params.errorMessage = 'Error Updating Book!!';
+            } else {
+                params.errorMessage = 'Error Creating Book!!';
+            }
+        }
+        res.render(`books/${form}.html`, params); //De esta forma hago dinamico la funcion
+    } catch {
+        res.redirect('/books');
     }
 };
 
